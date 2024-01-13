c00a57d5f6d190f9b70c50baa37e1aa2c7b60105
newtbot <limtiseng@gmail.com>
2024-01-14 02:43:27 +0800

    iot sensor finished
    1)with validation on front and backend
    2)fixed seed route generating value 0 for data

---
+++ IoT-sensor/Database/mySQL.js
@@ -0,0 +1,34 @@
+const dotenv = require("dotenv");
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') })
+const Sequelize = require("sequelize");
+const fs = require('fs');
+
+const sequelize = new Sequelize(
+ "eco_saver",
+ process.env.DB_USER,
+ process.env.DB_PASS,
+ {
+    host: "mpsqldatabase.mysql.database.azure.com",
+    dialect: 'mysql',
+    //  attributeBehavior?: 'escape' | 'throw' | 'unsafe-legacy';
+    attributeBehavior: 'escape',
+    dialectOptions: {
+      ssl: {
+         ca: fs.readFileSync(path.resolve(__dirname, '../../cert/DigiCertGlobalRootCA.crt.pem')),
+      },
+
+    },
+   },
+
+   
+);
+
+sequelize.authenticate().then(() => {
+   console.log('Connection has been established successfully.');
+}).catch((error) => {
+   console.error('Unable to connect to the database: ', error);
+});
+
+module.exports = { sequelize };
+
--- IoT-sensor/index.js
+++ IoT-sensor/index.js
@@ -1,34 +1,34 @@
-const { iot_sensor_data } = require("./modules/IoT-sensor");
+const { run } = require("./modules/IoT-sensor");
 const client = require("./modules/mqtt");
 
-function publishData() {
-    let data = iot_sensor_data();
-
-    // MQTT logic
-    client.publish("iot-data", JSON.stringify(data), { qos: 1 }, (err) => {
-        if (err) {
-            console.error("Error publishing message:", err);
-        } else {
-            console.log("Message published");
-        }
-    });
+async function publishData() {
+    try {
+        const data = await run();
+        console.log(data);
+        client.publish("iot-data", JSON.stringify(data));
+    } catch (err) {
+        console.error(err);
+    }
 }
 
 client.on("connect", () => {
-    console.log("Connected to MQTT broker");
-    publishData();
+	console.log("Connected to MQTT broker");
+	publishData();
 });
 
 client.on("end", () => {
-    console.log("Disconnected from MQTT broker");
-    client.reconnect = true;
+	console.log("Disconnected from MQTT broker");
+	client.reconnect = true;
 });
 
 client.on("error", (err) => {
-    console.error("Error:", err);
-    client.end(); 
+	console.error("Error:", err);
+	client.end();
 });
 
 //every 15 minutes
 setInterval(publishData, 900000);
-//setInterval(publishData, 600);
+//every 1 minute
+//setInterval(publishData, 60000);
+
+
