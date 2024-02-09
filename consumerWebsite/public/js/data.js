82fbb5d201cabbd5d0539eb8361bba5b36a9d3a0
newtbot <limtiseng@gmail.com>
2024-02-09 17:44:39 +0800

    Add services bar and legend to viewdata page

--- consumerWebsite/public/js/data.js
+++ consumerWebsite/public/js/data.js
@@ -12,6 +12,8 @@ $(document).ready(async function () {
 	var month = date.getMonth() + 1; // Adding 1 to get the actual month number
 	var year = date.getFullYear();
 
+	
+
 	// Initialize initialData for chart
 	const dailyinitialData = {
 		labels: [], // Array to store timestamps
@@ -48,10 +50,9 @@ $(document).ready(async function () {
 		],
 	};
 
-
-	// Create Chart.js chart
+	// https://www.chartjs.org/docs/latest/axes/
 	const ctx = document.getElementById("DailyDataChart").getContext("2d");
-	const chart = new Chart(ctx, {
+	const dailyChart = new Chart(ctx, {
 		type: "bar",
 		data: dailyinitialData,
 		options: {
@@ -60,9 +61,23 @@ $(document).ready(async function () {
 				display: true,
 				text: "Average measurement metric data by Hour",
 			},
+			scales: {
+				x: {
+					valueFormatString: "Time",
+					title: {
+						display: true,
+						text: "Date and Time in 24 hour format",
+					},
+				},
+				y: {
+					title: {
+						display: true,
+						text: "Measurement",
+					},
+				},
+			},
 		},
 	});
-
 	//weekly chart
 	const weeklyCtx = document.getElementById("WeeklyDataChart").getContext("2d");
 	const weeklyChart = new Chart(weeklyCtx, {
@@ -74,11 +89,28 @@ $(document).ready(async function () {
 				display: true,
 				text: "Average measurement metric data by Week",
 			},
+			scales: {
+				x: {
+					valueFormatString: "Time",
+					title: {
+						display: true,
+						text: "Week",
+					},
+				},
+				y: {
+					title: {
+						display: true,
+						text: "Measurement",
+					},
+				},
+			},
 		},
 	});
 
 	//monthly chart
-	const monthlyCtx = document.getElementById("MonthlyDataChart").getContext("2d");
+	const monthlyCtx = document
+		.getElementById("MonthlyDataChart")
+		.getContext("2d");
 	const monthlyChart = new Chart(monthlyCtx, {
 		type: "bar",
 		data: MonthlyinitialData,
@@ -87,6 +119,21 @@ $(document).ready(async function () {
 			title: {
 				display: true,
 				text: "Average measurement metric data by Month",
+			},
+			scales: {
+				x: {
+					valueFormatString: "Time",
+					title: {
+						display: true,
+						text: "Month",
+					},
+				},
+				y: {
+					title: {
+						display: true,
+						text: "Measurement",
+					},
+				},
 			},
 		},
 	});
@@ -122,7 +169,7 @@ $(document).ready(async function () {
 				const averageValue =
 					hourlyData[hourString].reduce((acc, val) => acc + val, 0) /
 					hourlyData[hourString].length;
-					dailyinitialData.labels.push(
+				dailyinitialData.labels.push(
 					new Date(hourString).toLocaleString("en-US", {
 						timeZone: "UTC",
 						hour12: false,
@@ -132,7 +179,7 @@ $(document).ready(async function () {
 			}
 
 			// Update chart
-			chart.update();
+			dailyChart.update();
 		});
 	}
 	function updateWeeklyChart(metric) {
@@ -141,7 +188,7 @@ $(document).ready(async function () {
 			// Clear previous data
 			weeklyinitialData.labels = []; //timestamp
 			weeklyinitialData.datasets[0].data = []; //measurement data depending on metric
-	
+
 			// Group data by week and calculate average value for each week
 			const weeklyData = {};
 			for (let row of data) {
@@ -151,18 +198,20 @@ $(document).ready(async function () {
 				}
 				weeklyData[weekNumber].push(row.measurement[metric]);
 			}
-	
+
 			for (let weekNumber in weeklyData) {
-				const averageValue = weeklyData[weekNumber].reduce((acc, val) => acc + val, 0) / weeklyData[weekNumber].length;
+				const averageValue =
+					weeklyData[weekNumber].reduce((acc, val) => acc + val, 0) /
+					weeklyData[weekNumber].length;
 				weeklyinitialData.labels.push(`Week ${weekNumber}`);
 				weeklyinitialData.datasets[0].data.push(averageValue);
 			}
-	
+
 			// Update chart
 			weeklyChart.update();
 		});
 	}
-	
+
 	function getWeekNumber(date) {
 		const onejan = new Date(date.getFullYear(), 0, 1);
 		const dayOfYear = Math.ceil((date - onejan) / 86400000);
@@ -188,7 +237,9 @@ $(document).ready(async function () {
 			}
 
 			for (let monthNumber in monthlyData) {
-				const averageValue = monthlyData[monthNumber].reduce((acc, val) => acc + val, 0) / monthlyData[monthNumber].length;
+				const averageValue =
+					monthlyData[monthNumber].reduce((acc, val) => acc + val, 0) /
+					monthlyData[monthNumber].length;
 				MonthlyinitialData.labels.push(`Month ${monthNumber}`);
 				MonthlyinitialData.datasets[0].data.push(averageValue);
 			}
@@ -197,7 +248,7 @@ $(document).ready(async function () {
 			monthlyChart.update();
 		});
 	}
-	
+
 	// Event listeners for buttons
 	document.getElementById("psiButton").addEventListener("click", function () {
 		updateChart("psi");
@@ -228,62 +279,88 @@ $(document).ready(async function () {
 	});
 
 	//week buttons
-	document.getElementById("weeklypsiButton").addEventListener("click", function () {
-		updateWeeklyChart("psi")
-	});
+	document
+		.getElementById("weeklypsiButton")
+		.addEventListener("click", function () {
+			updateWeeklyChart("psi");
+		});
 
-	document.getElementById("weeklytempButton").addEventListener("click", function () {
-		updateWeeklyChart("temperature");
-	});
+	document
+		.getElementById("weeklytempButton")
+		.addEventListener("click", function () {
+			updateWeeklyChart("temperature");
+		});
 
-	document.getElementById("weeklyhumButton").addEventListener("click", function () {
-		updateWeeklyChart("humidity");
-	});
+	document
+		.getElementById("weeklyhumButton")
+		.addEventListener("click", function () {
+			updateWeeklyChart("humidity");
+		});
 
-	document.getElementById("weeklyo3Button").addEventListener("click", function () {
-		updateWeeklyChart("o3");
-	});
+	document
+		.getElementById("weeklyo3Button")
+		.addEventListener("click", function () {
+			updateWeeklyChart("o3");
+		});
 
-	document.getElementById("weeklyno2Button").addEventListener("click", function () {
-		updateWeeklyChart("no2");
-	});
+	document
+		.getElementById("weeklyno2Button")
+		.addEventListener("click", function () {
+			updateWeeklyChart("no2");
+		});
 
-	document.getElementById("weeklyso2Button").addEventListener("click", function () {
-		updateWeeklyChart("so2");
-	});
+	document
+		.getElementById("weeklyso2Button")
+		.addEventListener("click", function () {
+			updateWeeklyChart("so2");
+		});
 
-	document.getElementById("weeklycoButton").addEventListener("click", function () {
-		updateWeeklyChart("co");
-	});
+	document
+		.getElementById("weeklycoButton")
+		.addEventListener("click", function () {
+			updateWeeklyChart("co");
+		});
 
 	//month buttons
-	document.getElementById("monthlypsiButton").addEventListener("click", function () {
-		updateMonthlyChart("psi");
-	});
+	document
+		.getElementById("monthlypsiButton")
+		.addEventListener("click", function () {
+			updateMonthlyChart("psi");
+		});
 
-	document.getElementById("monthlytempButton").addEventListener("click", function () {
-		updateMonthlyChart("temperature");
-	});
+	document
+		.getElementById("monthlytempButton")
+		.addEventListener("click", function () {
+			updateMonthlyChart("temperature");
+		});
 
-	document.getElementById("monthlyhumButton").addEventListener("click", function () {
-		updateMonthlyChart("humidity");
-	});
+	document
+		.getElementById("monthlyhumButton")
+		.addEventListener("click", function () {
+			updateMonthlyChart("humidity");
+		});
 
-	document.getElementById("monthlyo3Button").addEventListener("click", function () {
-		updateMonthlyChart("o3");
-	});
+	document
+		.getElementById("monthlyo3Button")
+		.addEventListener("click", function () {
+			updateMonthlyChart("o3");
+		});
 
-	document.getElementById("monthlyno2Button").addEventListener("click", function () {
-		updateMonthlyChart("no2");
-	});
+	document
+		.getElementById("monthlyno2Button")
+		.addEventListener("click", function () {
+			updateMonthlyChart("no2");
+		});
 
-	document.getElementById("monthlyso2Button").addEventListener("click", function () {
-		updateMonthlyChart("so2");
-	});
+	document
+		.getElementById("monthlyso2Button")
+		.addEventListener("click", function () {
+			updateMonthlyChart("so2");
+		});
 
-	document.getElementById("monthlycoButton").addEventListener("click", function () {
-		updateMonthlyChart("co");
-	});
-
-	
+	document
+		.getElementById("monthlycoButton")
+		.addEventListener("click", function () {
+			updateMonthlyChart("co");
+		});
 });
